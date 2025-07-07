5b2fd4ac8e680423db624ca9c3dde386beaa121d
Karina Vargas <vargasriosk@gmail.com>
2025-07-07 13:35:25 -0500

    add initial configuration and database setup with Docker support

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,32 @@
-import dotenv from "dotenv";
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
+import { Connection, createConnection } from "typeorm";
+
 import { UserRouter } from "./user/user.router";
+import { ConfigServer } from "./config/config";
 
-dotenv.config();
 
-class Server {
+class Server extends ConfigServer{
     public app: express.Application = express();
-    private port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+    private port: number = this.getNumberEnv("PORT") || 3000;
 
     constructor() {
+        super();
+
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
+
+
+        this.dbConnect()
+            .then((connection: Connection) => {
+                console.log("Database connected successfully:", connection);
+            })
+            .catch((error: Error) => {
+                console.error("Database connection failed:", error);
+            });
+
         this.app.use(morgan('dev'));
         this.app.use(cors());
 
@@ -27,6 +40,10 @@ class Server {
         return [
             new UserRouter().router
         ];
+    }
+
+    async dbConnect(): Promise<Connection> {
+        return await createConnection(this.TypeORMConfig)
     }
 
     public listen() {
