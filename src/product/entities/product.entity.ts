87011456d14f7737925547062d857ba069e5d844
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 11:25:40 -0500

    feat: change jsonb to json and array type to simple-array

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, ManyToOne, OneToMany, Index } from "typeorm";
 
 import { BaseEntity } from "../../config/base.entity";
-import { SellerEntity } from "user/entities/seller.entity";
+import { SellerEntity } from "../../user/entities/seller.entity";
 import { ProductCategoryEntity } from "./product_category.entity";
 import { ProductImageEntity } from "./product_image.entity";
 import { ProductReviewEntity } from "./product_review.entity";
@@ -9,61 +9,54 @@ import { IsInt, Max, Min } from "class-validator";
 
 @Entity("products")
 export class ProductEntity extends BaseEntity {
-	@Column({ type: "varchar", length: 255 })
-	name!: string;
+  @Column({ type: "varchar", length: 255, nullable: true })
+  name!: string;
 
-	@Column({ type: "text" })
-	description?: string;
+  @Column({ type: "text", nullable: true })
+  description?: string;
 
-	@Column({ type: "decimal", precision: 12, scale: 2, unsigned: true })
-	price!: number;
+  @Column({
+    type: "decimal",
+    precision: 12,
+    scale: 2,
+    unsigned: true,
+    nullable: true,
+  })
+  price!: number;
 
-	@Column({ type: "varchar", length: 100, unique: true })
-	@Index()
-	sku!: string;
+  @Column({ type: "varchar", length: 100, unique: true, nullable: true })
+  sku!: string;
 
-	@Column({ type: "int", unsigned: true })
-	stockQuantity!: number;
+  @Column({ type: "int", unsigned: true, nullable: true })
+  stockQuantity!: number;
 
-	@Column({ type: "decimal", precision: 8, scale: 3 })
-	weight?: number;
+  @Column({ type: "decimal", precision: 8, scale: 3 })
+  weight?: number;
 
-	@Column({ type: "jsonb" })
-	dimensions?: { length: number; width: number; height: number };
+  @Column({ type: "json" })
+  dimensions?: { length: number; width: number; height: number };
 
-	@Column({ type: "boolean", default: true })
-	isActive!: boolean;
+  @Column({ type: "boolean", default: true })
+  isActive!: boolean;
 
-	@Column({ type: "array" })
-	tags?: string[];
+  @Column({ type: "simple-array", nullable: true })
+  tags?: string[];
 
-	@IsInt()
-	@Min(0)
-	@Max(5)
-	@Column({ type: "int" })
-	ratingAvg?: number;
+  @IsInt()
+  @Min(0)
+  @Max(5)
+  @Column({ type: "int" })
+  ratingAvg?: number;
 
-	@ManyToOne(
-		() => SellerEntity,
-		(seller) => seller.products,
-	)
-	seller!: SellerEntity;
+  @ManyToOne(() => SellerEntity, (seller) => seller.products)
+  seller!: SellerEntity;
 
-	@ManyToOne(
-		() => ProductCategoryEntity,
-		(category) => category.products,
-	)
-	category!: ProductCategoryEntity;
+  @ManyToOne(() => ProductCategoryEntity, (category) => category.products)
+  category!: ProductCategoryEntity;
 
-	@OneToMany(
-		() => ProductImageEntity,
-		(image) => image.product,
-	)
-	images!: ProductImageEntity[];
+  @OneToMany(() => ProductImageEntity, (image) => image.product)
+  images!: ProductImageEntity[];
 
-	@OneToMany(
-		() => ProductReviewEntity,
-		(review) => review.product,
-	)
-	reviews!: ProductReviewEntity[];
+  @OneToMany(() => ProductReviewEntity, (review) => review.product)
+  reviews!: ProductReviewEntity[];
 }
