bb614c9848ff1d6792624b3605b92b11e4d9a0bb
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:08:54 -0500

    feat: define PaymentEntity with properties for order ID, amount, payment date, method, transaction ID, and status

--- src/payment/entities/payment.entity.ts
+++ src/payment/entities/payment.entity.ts
@@ -1,5 +1,22 @@
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity } from "typeorm";
 
 @Entity({ name: "payments" })
-export class PaymentEntity extends BaseEntity {}
+export class PaymentEntity extends BaseEntity {
+  orderId!: string; // OrderEntity
+
+  @Column()
+  amount!: number;
+
+  @Column()
+  paymentDate!: Date;
+
+  @Column()
+  paymentMethod!: string; // PaymentMethodEntity
+
+  @Column()
+  transaction_id!: string;
+
+  @Column()
+  status!: string;
+}
