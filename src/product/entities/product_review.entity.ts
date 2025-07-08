47c4cc51f67bba9fef0813338da36be966bef7f3
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:09:10 -0500

    feat: add customer relationship to ProductReviewEntity

--- src/product/entities/product_review.entity.ts
+++ src/product/entities/product_review.entity.ts
@@ -1,5 +1,9 @@
+import { CustomerEntity } from "user/entities/customer.entity";
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity({ name: "product_reviews" })
-export class ProductReviewEntity extends BaseEntity {}
+export class ProductReviewEntity extends BaseEntity {
+  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
+  customer!: CustomerEntity;
+}
