9c0f358b432d8f12ac288c8f9c1c35bcf263b1d2
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:09:25 -0500

    feat: define SellerEntity with businessName, taxId, and relationship to products

--- src/user/entities/seller.entity.ts
+++ src/user/entities/seller.entity.ts
@@ -1,5 +1,15 @@
+import { ProductEntity } from "product/entities/product.entity";
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, OneToMany } from "typeorm";
 
 @Entity({ name: "sellers" })
-export class SellerEntity extends BaseEntity {}
+export class SellerEntity extends BaseEntity {
+  @Column({ type: "varchar", length: 100 })
+  businessName!: string;
+
+  @Column({ type: "varchar", length: 50, unique: true })
+  taxId!: string;
+
+  @OneToMany(() => ProductEntity, (product) => product.seller)
+  products?: ProductEntity[];
+}
