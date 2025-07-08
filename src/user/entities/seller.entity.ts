18eb767e6c1602697f099dcf74821f6038922529
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 11:26:08 -0500

    feat: format code

--- src/user/entities/seller.entity.ts
+++ src/user/entities/seller.entity.ts
@@ -1,18 +1,15 @@
-import { ProductEntity } from "product/entities/product.entity";
+import { ProductEntity } from "../../product/entities/product.entity";
 import { BaseEntity } from "../../config/base.entity";
 import { Column, Entity, OneToMany } from "typeorm";
 
 @Entity({ name: "sellers" })
 export class SellerEntity extends BaseEntity {
-	@Column({ type: "varchar", length: 100 })
-	businessName!: string;
+  @Column({ type: "varchar", length: 100 })
+  businessName!: string;
 
-	@Column({ type: "varchar", length: 50, unique: true })
-	taxId!: string;
+  @Column({ type: "varchar", length: 50, unique: true })
+  taxId!: string;
 
-	@OneToMany(
-		() => ProductEntity,
-		(product) => product.seller,
-	)
-	products?: ProductEntity[];
+  @OneToMany(() => ProductEntity, (product) => product.seller)
+  products?: ProductEntity[];
 }
