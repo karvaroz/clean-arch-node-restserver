bc5781e07beefc61043dd29c2c14b0dfb7a1700c
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:09:19 -0500

    feat: update CustomerEntity to improve relationships and field definitions

--- src/user/entities/customer.entity.ts
+++ src/user/entities/customer.entity.ts
@@ -2,43 +2,48 @@ import { Column, Entity, JoinColumn, OneToMany, OneToOne } from "typeorm";
 import { BaseEntity } from "../../config/base.entity";
 import { UserEntity } from "user/entities/user.entity";
 import { OrderEntity } from "order/entities/order.entity";
+import { ShippingAddressEntity } from "order/entities/shipping_address.entity";
+import { PaymentMethodEntity } from "payment/entities/payment_method.entity";
+import { ProductReviewEntity } from "product/entities/product_review.entity";
+import { CartEntity } from "cart/entities/cart.entity";
+import { WishlistEntity } from "wishlist/entities/wishlist.entity";
 
 @Entity({ name: "customers" })
 export class CustomerEntity extends BaseEntity {
   @OneToOne(() => UserEntity, (user) => user.customer)
-  @JoinColumn({ name: "user_id" })
+  @JoinColumn({ name: "id" })
   user!: UserEntity;
 
   @Column({ type: "varchar", length: 50, nullable: true })
-  phoneNumber?: string;
+  phone?: string;
 
   @Column({ type: "date", nullable: true })
   birthDate?: Date;
 
   @Column({ type: "varchar", length: 20, unique: true, nullable: true })
-  identification?: string | null;
+  identification?: string;
 
-  @Column({ type: "varchar", length: 50 })
-  firstName?: string;
+  @Column({ type: "varchar", length: 50, nullable: true })
+  firstName!: string | null;
 
-  @Column({ type: "varchar", length: 50 })
-  lastName?: string;
+  @Column({ type: "varchar", length: 50, nullable: true })
+  lastName!: string | null;
 
-  // @OneToMany(() => AddressEntity, (address) => address.customer)
-  // addresses: AddressEntity[];
+  @OneToMany(() => ShippingAddressEntity, (address) => address.customer)
+  addresses?: ShippingAddressEntity[];
 
-  // @OneToMany(() => PaymentMethodEntity, (payment) => payment.customer)
-  // paymentMethods: PaymentMethodEntity[];
+  @OneToMany(() => PaymentMethodEntity, (payment) => payment.customer)
+  paymentMethods?: PaymentMethodEntity[];
 
-  // @OneToMany(() => OrderEntity, (order) => order.customer)
-  // orders: OrderEntity[];
+  @OneToMany(() => OrderEntity, (order) => order.customer)
+  orders?: OrderEntity[];
 
-  // @OneToMany(() => ReviewEntity, (review) => review.customer)
-  // reviews: ReviewEntity[];
+  @OneToMany(() => ProductReviewEntity, (review) => review.customer)
+  reviews?: ProductReviewEntity[];
 
-  // @OneToOne(() => ShoppingCartEntity, (cart) => cart.customer)
-  // shoppingCart: ShoppingCartEntity;
+  @OneToOne(() => CartEntity, (cart) => cart.customer)
+  shoppingCart?: CartEntity;
 
-  // @OneToMany(() => WishlistEntity, (wishlist) => wishlist.customer)
-  // wishlists: WishlistEntity[];
+  @OneToMany(() => WishlistEntity, (wishlist) => wishlist.customer)
+  wishlists?: WishlistEntity[];
 }
