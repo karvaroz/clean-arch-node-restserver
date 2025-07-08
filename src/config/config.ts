ce4180e0fc4b3a9ea7433a392e11d64e316e29cb
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 11:24:18 -0500

    feat: data source sync set to true

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,49 +4,50 @@ import { DataSourceOptions } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
 export abstract class ConfigServer {
-	constructor() {
-		const nodeNameEnv = this.createPathEnv(this.nodeEnv);
-		dotenv.config({
-			path: nodeNameEnv,
-		});
-	}
+  constructor() {
+    const nodeNameEnv = this.createPathEnv(this.nodeEnv);
+    dotenv.config({
+      path: nodeNameEnv,
+    });
+  }
 
-	public getEnvironment(k: string): string | undefined {
-		return process.env[k];
-	}
+  public getEnvironment(k: string): string | undefined {
+    return process.env[k];
+  }
 
-	public getNumberEnv(k: string): number {
-		return Number(this.getEnvironment(k));
-	}
+  public getNumberEnv(k: string): number {
+    return Number(this.getEnvironment(k));
+  }
 
-	public get nodeEnv(): string {
-		return this.getEnvironment("NODE_ENV")?.trim() || "";
-	}
+  public get nodeEnv(): string {
+    return this.getEnvironment("NODE_ENV")?.trim() || "";
+  }
 
-	public createPathEnv(path: string): string {
-		const arrEnv: Array<string> = ["env"];
+  public createPathEnv(path: string): string {
+    const arrEnv: Array<string> = ["env"];
 
-		if (path.length > 0) {
-			const stringToArray = path.split(".");
-			arrEnv.unshift(...stringToArray);
-		}
-		return "." + arrEnv.join(".");
-	}
+    if (path.length > 0) {
+      const stringToArray = path.split(".");
+      arrEnv.unshift(...stringToArray);
+    }
+    return "." + arrEnv.join(".");
+  }
 
-	public get TypeORMConfig(): DataSourceOptions {
-		return {
-			type: "mysql",
-			driver: require("mysql2"),
-			host: this.getEnvironment("DB_HOST"),
-			port: this.getNumberEnv("DB_PORT"),
-			username: this.getEnvironment("DB_USER"),
-			password: this.getEnvironment("DB_PASSWORD"),
-			database: this.getEnvironment("DB_DATABASE"),
-			entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-			migrations: [__dirname + "/../migrations/*{.ts,.js}"],
-			synchronize: true,
-			logging: false,
-			namingStrategy: new SnakeNamingStrategy(),
-		};
-	}
+  public get TypeORMConfig(): DataSourceOptions {
+    return {
+      type: "mysql",
+      driver: require("mysql2"),
+      host: this.getEnvironment("DB_HOST"),
+      port: this.getNumberEnv("DB_PORT"),
+      username: this.getEnvironment("DB_USER"),
+      password: this.getEnvironment("DB_PASSWORD"),
+      database: this.getEnvironment("DB_DATABASE"),
+      entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+      migrations: [__dirname + "/../migrations/*{.ts,.js}"],
+      synchronize: true,
+      migrationsRun: true,
+      logging: false,
+      namingStrategy: new SnakeNamingStrategy(),
+    };
+  }
 }
