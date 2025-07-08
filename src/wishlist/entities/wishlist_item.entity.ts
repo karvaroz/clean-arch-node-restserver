be7cce37827133f488c34edefd27e16ed61f84b6
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 11:26:31 -0500

    feat: fix imports

--- src/wishlist/entities/wishlist_item.entity.ts
+++ src/wishlist/entities/wishlist_item.entity.ts
@@ -1,16 +1,13 @@
 import { BaseEntity } from "../../config/base.entity";
 import { Entity, ManyToOne } from "typeorm";
 import { WishlistEntity } from "./wishlist.entity";
-import { ProductEntity } from "product/entities/product.entity";
+import { ProductEntity } from "../../product/entities/product.entity";
 
 @Entity({ name: "wishlist_items" })
 export class WishlistItemEntity extends BaseEntity {
-	@ManyToOne(
-		() => WishlistEntity,
-		(wishlist) => wishlist.items,
-	)
-	wishlist!: WishlistEntity;
+  @ManyToOne(() => WishlistEntity, (wishlist) => wishlist.items)
+  wishlist!: WishlistEntity;
 
-	@ManyToOne(() => ProductEntity)
-	product!: ProductEntity;
+  @ManyToOne(() => ProductEntity)
+  product!: ProductEntity;
 }
