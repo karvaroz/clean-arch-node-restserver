3ed111bef69036ae15552f3f3a0ac732ddd29d05
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:08:50 -0500

    feat: implement PaymentMethodEntity with customer relationship and payment type

--- src/payment/entities/payment_method.entity.ts
+++ src/payment/entities/payment_method.entity.ts
@@ -1,5 +1,26 @@
+import { PaymentType } from "payment/dto/payment_method.dto";
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
+import { CustomerEntity } from "user/entities/customer.entity";
 
 @Entity({ name: "payment_methods" })
-export class PaymentMethodEntity extends BaseEntity {}
+export class PaymentMethodEntity extends BaseEntity {
+  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
+  customer!: CustomerEntity;
+
+  @Column({
+    type: "enum",
+    enum: PaymentType,
+    default: PaymentType.CREDIT_CARD,
+  })
+  paymentType!: PaymentType;
+
+  @Column({ nullable: true })
+  card_number?: string;
+
+  @Column({ nullable: true })
+  card_holder_name?: string;
+
+  @Column({ nullable: true })
+  is_default?: boolean;
+}
