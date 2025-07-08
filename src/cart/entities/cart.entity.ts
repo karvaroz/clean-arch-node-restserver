064668ed95084aa98a65dcd468f3dee4d9c56ed6
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:08:20 -0500

    feat: add customer relationship to CartEntity

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,5 +1,9 @@
+import { CustomerEntity } from "user/entities/customer.entity";
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity({ name: "cart" })
-export class CartEntity extends BaseEntity {}
+export class CartEntity extends BaseEntity {
+  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
+  customer!: CustomerEntity;
+}
