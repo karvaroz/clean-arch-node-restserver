ce5027afe114cbc16960075f0bc4c842f36a2243
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 07:55:10 -0500

    feat: create ShippingMethodEntity for managing shipping methods

---
+++ src/order/entities/shipping_method.entity.ts
@@ -0,0 +1,14 @@
+import { BaseEntity } from "../../config/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity({ name: "shipping_methods" })
+export class ShippingMethodEntity extends BaseEntity {
+  @Column({ type: "varchar", length: 255 })
+  name!: string;
+
+  @Column({ type: "decimal", precision: 12, scale: 2 })
+  cost!: number;
+
+  @Column({ type: "text", nullable: true })
+  description?: string;
+}
