c086e951afbb2f475b390e7399c754d9df72ace3
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 11:24:51 -0500

    refactor: format code

--- src/payment/entities/payment_method.entity.ts
+++ src/payment/entities/payment_method.entity.ts
@@ -1,29 +1,26 @@
-import { PaymentType } from "payment/dto/payment_method.dto";
+import { PaymentType } from "../dto/payment_method.dto";
 import { BaseEntity } from "../../config/base.entity";
 import { Column, Entity, ManyToOne } from "typeorm";
 import { CustomerEntity } from "../../user/entities/customer.entity";
 
 @Entity({ name: "payment_methods" })
 export class PaymentMethodEntity extends BaseEntity {
-	@ManyToOne(
-		() => CustomerEntity,
-		(customer) => customer.orders,
-	)
-	customer!: CustomerEntity;
+  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
+  customer!: CustomerEntity;
 
-	@Column({
-		type: "enum",
-		enum: PaymentType,
-		default: PaymentType.CREDIT_CARD,
-	})
-	paymentType!: PaymentType;
+  @Column({
+    type: "enum",
+    enum: PaymentType,
+    default: PaymentType.CREDIT_CARD,
+  })
+  paymentType!: PaymentType;
 
-	@Column({ nullable: true, length: 19 })
-	cardNumber?: string;
+  @Column({ nullable: true, length: 19 })
+  cardNumber?: string;
 
-	@Column({ nullable: true })
-	cardholderName?: string;
+  @Column({ nullable: true })
+  cardholderName?: string;
 
-	@Column({ nullable: true })
-	isDefault?: boolean;
+  @Column({ nullable: true })
+  isDefault?: boolean;
 }
