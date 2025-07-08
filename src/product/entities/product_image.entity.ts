82ce5e08309471ff94fafe62c9fb0eb818d3c8ad
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:09:05 -0500

    feat: define ProductImageEntity with properties for URL, alt text, and relationship to ProductEntity

--- src/product/entities/product_image.entity.ts
+++ src/product/entities/product_image.entity.ts
@@ -1,5 +1,15 @@
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
+import { ProductEntity } from "./product.entity";
 
 @Entity({ name: "product_images" })
-export class ProductImageEntity extends BaseEntity {}
+export class ProductImageEntity extends BaseEntity {
+  @Column({ type: "varchar", length: 255 })
+  url!: string;
+
+  @Column({ type: "varchar", length: 50, nullable: true })
+  altText?: string;
+
+  @ManyToOne(() => ProductEntity, (product) => product.images)
+  product!: ProductEntity;
+}
