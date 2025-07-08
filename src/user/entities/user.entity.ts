8856400c72e312be2700fd6ea0bc2fad25138f45
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 07:56:02 -0500

    refactor: clean up UserEntity by removing unused fields and ensuring role is required

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,11 +1,8 @@
-import { Column, Entity } from "typeorm";
-import { BaseEntity } from "../../config/base.entity";
+import { Column, Entity, OneToOne } from "typeorm";
 
-export enum UserRole {
-  ADMIN = "admin",
-  EDITOR = "editor",
-  CUSTOMER = "customer",
-}
+import { BaseEntity } from "../../config/base.entity";
+import { CustomerEntity } from "user/entities/customer.entity";
+import { UserRole } from "user/dto/user.dto";
 
 @Entity({ name: "users" })
 export class UserEntity extends BaseEntity {
@@ -15,25 +12,15 @@ export class UserEntity extends BaseEntity {
   @Column({ type: "varchar", length: 255 })
   password!: string;
 
-  @Column({ type: "varchar", length: 50 })
-  firstName?: string;
-
-  @Column({ type: "varchar", length: 50 })
-  lastName?: string;
-
-  @Column({ type: "varchar", length: 15, nullable: true })
-  phoneNumber?: string;
-
   @Column({ type: "boolean", default: true })
   isActive!: boolean;
 
   @Column({ type: "boolean", default: false })
   isEmailVerified!: boolean;
 
-  @Column({
-    type: "enum",
-    enum: UserRole,
-    default: UserRole.CUSTOMER,
-  })
+  @Column({ type: "enum", enum: UserRole, nullable: false })
   role!: UserRole;
+
+  @OneToOne(() => CustomerEntity, (customer) => customer.user)
+  customer!: CustomerEntity;
 }
