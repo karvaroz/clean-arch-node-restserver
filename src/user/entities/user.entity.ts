3e3926bacd34af8d4a475b9b5fbfedca5ce3b1c0
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 11:26:20 -0500

    feat: fix imports

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,30 +2,27 @@ import { Column, Entity, OneToOne } from "typeorm";
 
 import { BaseEntity } from "../../config/base.entity";
 import { CustomerEntity } from "../../user/entities/customer.entity";
-import { UserRole } from "user/dto/user.dto";
+import { UserRole } from "../dto/user.dto";
 import { IsEmail } from "class-validator";
 
 @Entity({ name: "users" })
 export class UserEntity extends BaseEntity {
-	@IsEmail()
-	@Column({ type: "varchar", length: 100, unique: true })
-	email!: string;
+  @IsEmail()
+  @Column({ type: "varchar", length: 100, unique: true })
+  email!: string;
 
-	@Column({ type: "varchar", length: 255 })
-	passwordHash!: string;
+  @Column({ type: "varchar", length: 255 })
+  passwordHash!: string;
 
-	@Column({ type: "boolean", default: true })
-	isActive!: boolean;
+  @Column({ type: "boolean", default: true })
+  isActive!: boolean;
 
-	@Column({ type: "boolean", default: false })
-	emailVerified!: boolean;
+  @Column({ type: "boolean", default: false })
+  emailVerified!: boolean;
 
-	@Column({ type: "enum", enum: UserRole, nullable: false })
-	role!: UserRole;
+  @Column({ type: "enum", enum: UserRole, nullable: false })
+  role!: UserRole;
 
-	@OneToOne(
-		() => CustomerEntity,
-		(customer) => customer.user,
-	)
-	customer!: CustomerEntity;
+  @OneToOne(() => CustomerEntity, (customer) => customer.user)
+  customer!: CustomerEntity;
 }
