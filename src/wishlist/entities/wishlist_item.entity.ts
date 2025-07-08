c99c10b3cb38e845918935bd27142398dc2f0288
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:09:38 -0500

    feat: implement WishlistItemEntity with customer, wishlistId, and productId fields

--- src/wishlist/entities/wishlist_item.entity.ts
+++ src/wishlist/entities/wishlist_item.entity.ts
@@ -1,5 +1,15 @@
+import { CustomerEntity } from "user/entities/customer.entity";
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity({ name: "wishlist_item" })
-export class WishlistItemEntity extends BaseEntity {}
+export class WishlistItemEntity extends BaseEntity {
+  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
+  customer!: CustomerEntity;
+
+  @Column({ type: "varchar", length: 255 })
+  wishlistId!: string; // WishlistEntity
+
+  @Column({ type: "varchar", length: 255 })
+  productId!: string; // ProductEntity
+}
