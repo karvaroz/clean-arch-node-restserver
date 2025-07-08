11719248f3ef599e920e950441420c0cc0116570
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:09:43 -0500

    feat: expand WishlistEntity with customer relationship and additional fields

--- src/wishlist/entities/wishlist.entity.ts
+++ src/wishlist/entities/wishlist.entity.ts
@@ -1,5 +1,21 @@
+import { CustomerEntity } from "user/entities/customer.entity";
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity({ name: "wishlist" })
-export class WishlistEntity extends BaseEntity {}
+export class WishlistEntity extends BaseEntity {
+  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
+  customer!: CustomerEntity;
+
+  @Column({ type: "varchar", length: 255 })
+  name!: string;
+
+  @Column({ type: "text", nullable: true })
+  description?: string;
+
+  @Column({ type: "boolean", default: false })
+  isPublic!: boolean;
+
+  @Column({ type: "boolean", default: false })
+  isDefault!: boolean;
+}
