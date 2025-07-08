e22d2e51c931946b95a08d08ec1c31209837023c
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:27:54 -0500

    feat: add relation to cart items entity

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,9 +1,13 @@
 import { CustomerEntity } from "user/entities/customer.entity";
 import { BaseEntity } from "../../config/base.entity";
-import { Column, Entity, ManyToOne } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
+import { CartItemEntity } from "./cart_item.entity";
 
 @Entity({ name: "cart" })
 export class CartEntity extends BaseEntity {
   @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
   customer!: CustomerEntity;
+
+  @OneToMany(() => CartItemEntity, (item) => item.cart)
+  items!: CartItemEntity[];
 }
