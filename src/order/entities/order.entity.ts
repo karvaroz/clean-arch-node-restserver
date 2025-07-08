da6a930ed2ac43436aea29330a0b171889204664
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:16:44 -0500

    feat: remove unused importm and add customer  order relationship

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,20 +1,27 @@
-import { Column, ManyToOne } from "typeorm";
+import { Column, ManyToOne, OneToMany } from "typeorm";
 import { BaseEntity } from "../../config/base.entity";
 import { OrderType } from "order/dto/order.dto";
 import { CustomerEntity } from "user/entities/customer.entity";
+import { OrderItemEntity } from "./order_item.entity";
+import { PaymentEntity } from "payment/entities/payment.entity";
+import { ShippingAddressEntity } from "./shipping_address.entity";
+import { ShippingMethodEntity } from "./shipping_method.entity";
 
 export class OrderEntity extends BaseEntity {
-  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
-  customer!: CustomerEntity;
-
   @Column({ type: "datetime", length: 255 })
   orderDate!: Date;
 
   @Column({ type: "decimal", precision: 12, scale: 2 })
-  total!: number;
+  subtotal!: number;
 
-  @Column({ type: "decimal", precision: 12, scale: 2, default: 0 })
+  @Column({ type: "decimal", precision: 12, scale: 2 })
+  tax!: number;
+
+  @Column({ type: "decimal", precision: 12, scale: 2 })
   shippingCost!: number;
+
+  @Column({ type: "decimal", precision: 12, scale: 2 })
+  total!: number;
 
   @Column({
     type: "enum",
@@ -22,4 +29,19 @@ export class OrderEntity extends BaseEntity {
     default: OrderType.PROCESSING,
   })
   status!: OrderType;
+
+  @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
+  customer!: CustomerEntity;
+
+  @OneToMany(() => OrderItemEntity, (item) => item.order)
+  items!: OrderItemEntity[];
+
+  @OneToMany(() => PaymentEntity, (payment) => payment.orderId)
+  payments!: PaymentEntity[];
+
+  @ManyToOne(() => ShippingAddressEntity)
+  shippingAddress!: ShippingAddressEntity;
+
+  @ManyToOne(() => ShippingMethodEntity)
+  shippingMethod!: ShippingMethodEntity;
 }
