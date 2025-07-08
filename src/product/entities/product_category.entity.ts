47ae9c5b99dba60ce6653ecdfe5ec845e318c63f
Karina Vargas <vargasriosk@gmail.com>
2025-07-08 09:08:59 -0500

    feat: define ProductCategoryEntity with properties for name, description, and relationship to products

--- src/product/entities/product_category.entity.ts
+++ src/product/entities/product_category.entity.ts
@@ -1,5 +1,15 @@
 import { BaseEntity } from "../../config/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, OneToMany } from "typeorm";
+import { ProductEntity } from "./product.entity";
 
 @Entity({ name: "product_categories" })
-export class ProductCategoryEntity extends BaseEntity {}
+export class ProductCategoryEntity extends BaseEntity {
+  @Column({ type: "varchar", length: 100 })
+  name!: string;
+
+  @Column({ type: "text", nullable: true })
+  description?: string;
+
+  @OneToMany(() => ProductEntity, (product) => product.category)
+  products!: ProductEntity[];
+}
